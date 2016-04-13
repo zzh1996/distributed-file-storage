c27764715817b0027e99c44c29d896a613d2c400
Knight <Knight42@mail.ustc.edu.cn>
2016-04-13 12:20:38 +0800

    change default value

--- webui/templates/app.js
+++ webui/templates/app.js
@@ -1,9 +1,9 @@
-;(function() {
+<script type="text/javascript">
+(function() {
   'use strict'
 
-  const ws = new WebSocket('ws://localhost:8000/ws')
+  const ws = new WebSocket('ws://localhost:{{ port }}/ws')
   ws.onmessage = function(evt) {
-    //console.dir(evt)
     let data
     try {
       data = JSON.parse(evt.data)
@@ -11,15 +11,16 @@
       data = {}
     }
 
+    console.dir(data)
     switch (data.event) {
       case 'connect':
         //console.log(`new client from ${data.addr}:${data.port}`)
         $('#notification').text(`connection from ${data.host}:${data.port}`)
-        $('#notification').fadeTo(1000, 500).slideUp(500)
+        $('#notification').fadeTo(2000, 500).slideUp(500)
         break
       case 'upload-success':
         $('#notification').text(`successfully receive ${data.name}`)
-        $('#notification').fadeTo(1000, 500).slideUp(500)
+        $('#notification').fadeTo(2500, 500).slideUp(500)
         break
       default:
         console.log('new data', data)
@@ -27,3 +28,4 @@
   }
 
 })()
+</script>
--- webui/test/test_upload.py
+++ webui/test/test_upload.py
@@ -4,11 +4,14 @@ from __future__ import print_function, unicode_literals, with_statement, divisio
 
 import socket
 import json
+import sys
 import os
 
+f = sys.argv[1] or 'app.py'
 s = socket.socket()
 s.connect(('127.0.0.1', 18000))
-content = open('app.py', 'rb').read()
-size = os.path.getsize('app.py')
-d = json.dumps(dict(size=size, method='upload', name='app.py')).encode()
+content = open(f, 'rb').read()
+size = os.path.getsize(f)
+name = os.path.basename(f)
+d = json.dumps(dict(size=size, method='upload', name=name)).encode()
 s.send(d + content)
