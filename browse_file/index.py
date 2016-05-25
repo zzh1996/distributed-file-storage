cac62ed14d60c696f65baca8ee2a866d5e9f6342
Knight <Knight42@mail.ustc.edu.cn>
2016-05-25 16:24:26 +0800

    index.py: append slash after dirpath && pep8 format

--- browse_file/index.py
+++ browse_file/index.py
@@ -4,7 +4,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 
-import os,sys
+import os
+import sys
 import pathlib
 
 app = Flask(__name__)
@@ -12,30 +13,36 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 Bootstrap(app)
 
+
 @app.route('/list')
 def list_dir():
     if request.args.get('local_path'):
-        div=0
+        div = 0
         arg = request.args.get('local_path')
         path = pathlib.Path(arg)
-        filelist=[('..',str(path.parent),0)]
+        filelist = [('..', str(path.parent), 0)]
         for f in path.iterdir():
             if f.is_dir():
-                filelist.append((f.name,str(f),0))
+                filelist.append((f.name, str(f), 0))
             else:
-                filelist.append((f.name,str(f),1))
+                filelist.append((f.name, str(f), 1))
     elif request.args.get('remote_path'):
-        div=1
+        div = 1
         arg = request.args.get('remote_path')
         path = pathlib.Path(arg)
-        filelist=[('..',str(path.parent),0)]
+        filelist = [('..', str(path.parent), 0)]
         for f in path.iterdir():
             if f.is_dir():
-                filelist.append((f.name,str(f),0))
+                filelist.append((f.name, str(f), 0))
             else:
-                filelist.append((f.name,str(f),1))
-    filelist.sort(key=lambda f:(f[2],f[0]))
-    return render_template('viewfiles.html',curr_path=str(path),filelist=filelist,div=div)
+                filelist.append((f.name, str(f), 1))
+    filelist.sort(key=lambda f: (f[2], f[0]))
+    curr_path = str(path)
+    if not curr_path.endswith(os.sep):
+        curr_path += os.sep
+    return render_template(
+        'viewfiles.html', curr_path=curr_path, filelist=filelist, div=div)
+
 
 @app.route('/')
 def index():
@@ -43,4 +50,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-
