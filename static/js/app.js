b60752f3d628015dd3f12777d203ddbea7bf3304
Knight <Knight42@mail.ustc.edu.cn>
2016-06-25 20:46:26 +0800

    Notify user if error occurred

--- static/js/app.js
+++ static/js/app.js
@@ -11,14 +11,27 @@ function clickdir(f, div) {
   }
 }
 
+function showErr(e) {
+  var msg = '\
+  <div class="alert alert-danger">\
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+      <strong>Error!</strong>' + e +
+  '</div>'
+  $('.container').prepend(msg)
+}
+
 function getlocal(f) {
-  $.get('/list', {local_path: f}, function(data) {
+  $.get('/list', {
+    local_path: f
+  }, function(data) {
     $('#local-files').html(data);
   });
 }
 
 function getremote(f) {
-  $.get('/list', {remote_path: f}, function(data) {
+  $.get('/list', {
+    remote_path: f
+  }, function(data) {
     $('#remote-files').html(data);
   });
 }
@@ -30,18 +43,25 @@ function download(evt) {
   if (download.length > 0) {
     var dlFiles = JSON.stringify(download)
     var localRoot = $('#local-files h3').text()
-    //console.dir($('.active'));
-    //console.log(dlFiles)
-    //console.log(localRoot)
+    $('#download-btn').addClass('disabled')
     $.post('/download', {
       downloadlist: dlFiles,
       localpath: localRoot,
-    }, function(data) {
+    })
+    .done(function(data) {
       getlocal(localRoot)
       Array.prototype.slice.call($('#remote-files .active')).forEach(function(ele) {
         $(ele).toggleClass('active');
-      });
+      })
     })
+    .fail(function(e) {
+      //console.dir(e)
+      showErr('Failed to download all files')
+    })
+    .always(function() {
+      $('#download-btn').removeClass('disabled')
+    });
+
   } else {
     alert('Please select files!');
   }
@@ -52,18 +72,24 @@ function upload(evt) {
     return e.getAttribute('fullpath')
   })
   if (uploadlist.length > 0) {
-    //console.log(JSON.stringify(uploadlist));
-    //console.log($('#remote-files h3').text());
     var remotepath = $('#remote-files h3').text();
+    $('#upload-btn').addClass('disabled')
     $.post('/upload', {
-      uploadlist:JSON.stringify(uploadlist),
-      remotepath:remotepath
-    }, function(data) {
+      uploadlist: JSON.stringify(uploadlist),
+      remotepath: remotepath
+    })
+    .done(function(data) {
       getremote(remotepath);
       Array.prototype.slice.call($('#local-files .active')).forEach(function(ele) {
         $(ele).toggleClass('active');
       });
-    });
+    })
+    .fail(function(e) {
+      showErr('Failed to upload files')
+    })
+    .always(function() {
+      $('#upload-btn').removeClass('disabled')
+    })
   } else {
     alert('Please select files!');
   }
@@ -72,23 +98,29 @@ function upload(evt) {
 function sync(evt) {
   if (confirm('Are you sure to synchronize all changes?')) {
     var remotepath = $('#remote-files h3').text();
-    $.post('/sync', {}, function(data) {
+    $.post('/sync').done(function(data) {
       var dialog = $('#progressdialog');
       dialog.modal('show');
       var task = setInterval(function() {
-        $.get('/status', {}, function(data) {
+        $.get('/status').done(function(data) {
           var status = JSON.parse(data);
           var percent = 0;
           var text = '';
           var objname = '';
           if (status['upload_file_num'] != status['uploaded_file_num']) {
             percent = status['uploaded_file_num'] / status['upload_file_num'];
-            text = 'Uploading files...(' + status['uploaded_file_num'] + '/' + status['upload_file_num'] + ') ';
             objname = status['uploading_file_name'];
+            text = 'Uploading files...(' +
+                    status['uploaded_file_num'] +
+                    '/' +
+                    status['upload_file_num'] + ') ';
           } else if (status['uploaded_index_num'] != status['upload_index_num']) {
             percent = status['uploaded_index_num'] / status['upload_index_num'];
-            text = 'Uploading index...(' + status['uploaded_index_num'] + '/' + status['upload_index_num'] + ') ';
             objname = status['uploading_index_name'];
+            text = 'Uploading index...(' +
+                    status['uploaded_index_num'] +
+                    '/' +
+                    status['upload_index_num'] + ') ';
           } else {
             clearInterval(task);
             dialog.modal('hide');
@@ -98,9 +130,20 @@ function sync(evt) {
           $('.progress-bar').css('width', percent + '%').attr('aria-valuenow', percent);
           $('#progresstext').text(text);
           $('#objname').text(objname);
-        });
-      }, 500);
-    });
+
+        }).fail(function(e) {
+        // FIXME
+          console.dir(e)
+          clearInterval(task)
+          dialog.modal('hide');
+          showErr('Failed to retrive status')
+        })
+      }, 500)
+
+    // FIXME
+    }).fail(function() {
+      showErr('Failed to sync')
+    })
   }
 }
 
@@ -112,12 +155,16 @@ function deleteFiles(evt) {
     if (confirm('Are you sure to delete these files?')) {
       var remotepath = $('#remote-files h3').text();
       $.post('/delete', {
-        deletelist:JSON.stringify(deletelist),
-        remotepath:remotepath
-      }, function(data) {
+        deletelist: JSON.stringify(deletelist),
+        remotepath: remotepath
+      })
+      .done(function(data) {
         getremote(remotepath);
-        //alert(data);
-      });
+      })
+      // FIXME
+      .fail(function() {
+        showErr('Failed to delete')
+      })
     }
   } else {
     alert('Please select files!');
