5d7256658ceccd69b2e493db7ba5b2eebce77036
Lewis <sami@mail.ustc.edu.cn>
2016-06-28 03:10:34 +0800

    测试文件增加导入导出公钥功能的说明

--- vpath/meow.py
+++ vpath/meow.py
@@ -9,25 +9,36 @@ input_data= {
     'name_email':'test_sami@mydomain.com',
     'passphrase': 'Operation'
 }
-key_pair = gpg_wrapper.gpg_key('/tmp/gpghome1')
+input_data1= {
+    'key_type' : 'RSA',
+    'key_length': 1024,
+    'key_usage': 'ESCA',
+    'subkey_type':'RSA',
+    'subkey_length':1024,
+    'subkey_usage':'encrypt,sign,auth',
+    'name_email':'test@mydomain.com',
+    'passphrase': 'Security'
+}
+key_pair = gpg_wrapper.gpg_key('gpghome1')
 #key_pair.delete_key('test@mydomain.com')
 #key_pair.generate_key(input_data)
 #key_pair.list_public_keys()
-public_key = key_pair.export_public_key('test_sami@mydomain.com')
+#public_key = key_pair.export_public_key('test_sami@mydomain.com')
 
-gpg = gpg_wrapper.gpg_key('/tmp/gpghome')
+gpg = gpg_wrapper.gpg_key('gpghome')
 #gpg.delete_key('test@mydomain.com')
-gpg.import_public_key(public_key)
+#gpg.import_public_key(public_key)
+#gpg.generate_key(input_data1)
 #gpg.list_public_keys()
 
 
-gpg= gpg_wrapper.gpg_object('/tmp/gpghome', 'sami@mydomain.com', 'passphrase')
+gpg= gpg_wrapper.gpg_object('gpghome', 'test@mydomain.com', 'Security')
 fingerprint, hash, passphrase= \
     gpg_wrapper.blockchain_encrypt(
         b'I wish you a Merry Christmas',
-        '/tmp/gpghome',
-        'sami@mydomain.com',
-        'passphrase'
+        'gpghome',
+        'test@mydomain.com',
+        'Security'
     )
 de_passphrase= gpg.decrypt_assym_message(passphrase)
 print(de_passphrase)
@@ -36,3 +47,4 @@ print(de_hash)
 
 signature= gpg.sign_message(b'Decorate a Christmas tree')
 print(gpg.verify_signature(b'Decorate a Christmas tree', signature))
+
